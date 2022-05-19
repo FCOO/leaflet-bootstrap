ce0e4c83d63691e6a496ee8593d96cdace8b3eca
Niels Holt <niels@steenbuchholt.dk>
2022-05-19 19:56:22 +0200

    Bug fix from 3.24.0 New default options for BsLegend at L.BsLegend_defaultOptions

--- src/45_leaflet-bootstrap-control-legend.js
+++ src/45_leaflet-bootstrap-control-legend.js
@@ -211,8 +211,24 @@ leaflet-bootstrap-control-legend.js
 
     *******************************************************************
     ******************************************************************/
+    L.BsLegend_defaultOptions = {
+        show       : true,  //Show or hide the legend at init
+        showContent: true,  //Show or hide the content at init
+        showIcons  : true,  //Show or hide the icon-buttons t the header at init
+        isExtended : true,  //Extend/diminish the legend at init
+
+        //closeIconOptions = options for the close-icon in the header that removes the layer
+        closeIconOptions: {
+            icon     : ['fa-map fa-scale-x-08', 'fa-slash fa-scale-x-08'],
+            className: 'fa-map-margin-right',
+            title    : {da:'Skjul', en:'Hide'},
+        }
+    };
+
+
     L.BsLegend = function( options ){
-        this.options = $.extend(this.options, options);
+        this.options = $.extend(true, L.BsLegend_defaultOptions, options);
+
         this.index = options.index;
     };
 
